d3204479796ea2f3d46082b4a5658d5df28745b8
Vladimir <sergeev24085@mail.ru>
2020-06-02 23:18:15 +0700

    initial

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./styles/main.pcss";
+
+if (process.env.NODE_ENV === "development") {
+  require("file-loader!./index.pug");
+}
+
+import "slick-carousel";
+import "./scripts/slider";
---
+++ src/scripts/slider.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('.slider').slick({
+    arrows: false,
+    vertical: true,
+    dots: true,
+    dotsClass: 'slider-dots',
+    autoplay: 2000,
+    verticalSwiping: true
+  });
+});
