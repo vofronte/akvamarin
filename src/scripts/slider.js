078ef007cc7d4234824e123e5e94da78dd9e3de8
Vladimir <sergeev24085@mail.ru>
2020-06-04 23:18:12 +0700

    started slider

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,10 +1,9 @@
 $(function() {
-  $('.slider').slick({
+  $('.home-slider').slick({
     arrows: false,
-    vertical: true,
-    dots: true,
-    dotsClass: 'slider-dots',
-    autoplay: 2000,
-    verticalSwiping: true
+    // dots: true,
+    // dotsClass: 'slider-dots',
+    // fade: true,
+    variableWidth: true,
   });
 });
