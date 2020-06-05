42a4dc44c362b60ac0fc18096544910f685c076e
Vladimir Sergeev <sergeev24085@mail.ru>
2020-06-05 17:08:34 +0700

    finishes home slick-slider

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,9 +1,17 @@
 $(function() {
+
+  $('.home-slider').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $('.counter__pages-text_bold').text(i.toString().padStart(2, '0'));
+    $('.counter__pages-text').text('/' + slick.slideCount.toString().padStart(2, '0'));
+});
+
+
   $('.home-slider').slick({
-    arrows: false,
-    // dots: true,
-    // dotsClass: 'slider-dots',
-    // fade: true,
-    variableWidth: true,
+    arrows: true,
+    prevArrow: $('.button-slder_prev'),
+    nextArrow: $('.button-slder_next'),
+    fade: true
   });
 });
